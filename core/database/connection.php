00115094057b6b853a424354b59e46253434587c
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-02 21:43:56 +0200

    Baza danych

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,16 +1,16 @@
 <?php
 // useless connection before i try to use class 
 
-// $servername = "localhost";
-// $db_name="twitter";
-// $username = "root";
-// $password = "";
+$servername = "172.93.105.132";
+$db_name="db_los_angos_dc";
+$username = "root";
+$password = "dIpG01c_*ssaB0ai0A+EckjzzMGf@b9A75C1E@#Az";
 
-// try {
-//   $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
-//   // set the PDO error mode to exception
-//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// //   echo "Connected successfully";
-// } catch(PDOException $e) {
-//   echo "Connection failed: " . $e->getMessage();
-// }
+try {
+  $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+  // set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
